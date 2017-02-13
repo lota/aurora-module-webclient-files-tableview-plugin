a381a129eed72132509234a69c24799049ac1a97
sash04ek <a.ovcharov@gmail.com>
2017-02-13 08:44:56 +0300

    updated

--- js/manager.js
+++ js/manager.js
@@ -12,7 +12,8 @@ module.exports = function (oAppData) {
 		Settings = require('modules/%ModuleName%/js/Settings.js'),
 		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {}),
 		
-		bNormalUser = App.getUserRole() === window.Enums.UserRole.NormalUser
+		bNormalUser = App.getUserRole() === window.Enums.UserRole.NormalUser,
+		bShow = false
 	;
 
 	Settings.init(oSettings);
@@ -41,30 +42,34 @@ module.exports = function (oAppData) {
 							$Iframe = $('<iframe id="view_iframe" name="view_iframe" style="width: 100%; height: 100%; border: none;"></iframe>')
 						;
 						
-						$Iframe.load(function(){
-							$('iframe').contents().find('img').css({'max-width':'100%', 'max-height':'100%'});							
-						});
+						if (!bShow)
+						{
+							bShow = true;
+							$Iframe.load(function(){
+								$Iframe.contents().find('img').css({'max-width':'100%', 'max-height':'100%'});							
+							});
 
-//							var $oRightPannel = $('<div id="files_right_panel" style="width: 480px; border-left: 1px solid #eee;" data-bind="text: displayName"></div>');
-						
-						$RightPannel.append($Form);
-						$RightPannel.append($Iframe);
-						
-						$('<input type="hidden" name="Format" />').val('Raw').appendTo($Form);
-						$('<input type="submit" />').val('submit').appendTo($Form);
-						$("#files_center_panel").after($RightPannel);
+	//							var $oRightPannel = $('<div id="files_right_panel" style="width: 480px; border-left: 1px solid #eee;" data-bind="text: displayName"></div>');
 
-						oParams.View.firstSelectedFile.subscribe(function(newValue) {
-							$Iframe.attr('src', "");								
-							if (newValue !== undefined && oItem !== newValue)
-							{
-								newValue.createFormFields($Form, 'ViewFile');
-								$Form.submit();									
-//									data.displayName(newValue.displayName());
-							}
-						});
+							$RightPannel.append($Form);
+							$RightPannel.append($Iframe);
 
-						ko.applyBindings(data, $RightPannel.get(0));
+							$('<input type="hidden" name="Format" />').val('Raw').appendTo($Form);
+							$('<input type="submit" />').val('submit').appendTo($Form);
+							$("#files_center_panel").after($RightPannel);
+
+							oParams.View.firstSelectedFile.subscribe(function(newValue) {
+								$Iframe.attr('src', "");								
+								if (newValue !== undefined && oItem !== newValue)
+								{
+									newValue.createFormFields($Form, 'ViewFile');
+									$Form.submit();									
+	//									data.displayName(newValue.displayName());
+								}
+							});
+
+							ko.applyBindings(data, $RightPannel.get(0));
+						}
 					});
 				}
 
