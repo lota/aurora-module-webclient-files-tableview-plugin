aafe13936fb828a91dd4c8b00b8c66ceb664f672
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:47:24 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\FilesTableviewWebclientPlugin;
 
-class FilesTableviewWebclientPluginModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	public function init() 
 	{
