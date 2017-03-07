c29354c5b48211fdfddc0df2aaf52dc0ad440324
sash04ek <a.ovcharov@gmail.com>
2017-03-07 11:22:21 +0300

    added

--- Module.php
+++ Module.php
@@ -2,7 +2,7 @@
 
 namespace Aurora\Modules\FilesTableviewWebclientPlugin;
 
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	public function init() 
 	{
