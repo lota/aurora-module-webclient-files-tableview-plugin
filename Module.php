a78e543b2094776564018253fd889f68d9c16e1f
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:37:49 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\FilesTableviewWebclientPlugin;
 
 /**
+ * Replaces folders and files list view template in Files module for displaying items in table.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
